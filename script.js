455e010ca43c75fd76e7350e846ff28968c62996
Vedant Vatsa <26vedu@gmail.com>
2023-09-26 10:21:08 +0530

    Web APP

---
+++ script.js
@@ -0,0 +1,174 @@
+import { getDatabase, ref, push, onValue, set } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+
+import { firebaseConfig, app } from './Firebase/firebase.js';
+// Initialize Firebase using the provided configuration
+firebase.initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const loginForm = document.querySelector(".form.login");
+const signupForm = document.querySelector(".form.signup");
+const userUid = sessionStorage.getItem("user");
+const userTasksRef = ref(database, `${userUid}/tasks`);
+
+// Login form submission
+// Login form submission
+loginForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  let email = loginForm.querySelector(".email").value;
+  let password = loginForm.querySelector(".password").value;
+
+  // Authenticate the user with Firebase Auth
+  // Inside script.js (for email/password sign-in)
+
+// After successful login
+// ...
+
+  // After successful login
+firebase.auth().signInWithEmailAndPassword(email, password)
+.then(function(userCredential) {
+    // Get the user's UID
+    const userUid = userCredential.user.uid;
+    // Get the user's email
+    const userEmail = userCredential.user.email;
+
+    // Determine the reference based on authentication method
+    const userTasksRef = userEmail.includes('@gmail.com')
+        ? ref(database, `googleUsers/${userUid}/tasks`)
+        : ref(database, `emailUsers/${userUid}/tasks`);
+
+    // Fetch the existing tasks from the database
+    fetchTasks(userUid);
+
+    // Store the user's UID in session storage
+    sessionStorage.setItem("user", userUid); // Set the same key "user" for both login methods
+    sessionStorage.setItem("userEmail", userEmail);
+
+    // Redirect to the desired page
+    window.location.href = "to-do.html";
+})
+.catch(function(error) {
+    // Handle login errors
+    alert(error.message);
+});
+
+
+
+});
+
+
+// Signup form submission
+signupForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  let email = signupForm.querySelector(".email").value;
+  let password = signupForm.querySelector(".password").value;
+  let confirmPassword = signupForm.querySelector(".password").value;
+
+  // Check if the passwords match
+  if (password !== confirmPassword) {
+    alert("Passwords do not match!");
+    return;
+  }
+
+  // Create a new user with Firebase Auth
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user) {
+    // The user has been created successfully
+    window.location.href = "/";
+  }).catch(function(error) {
+    // The signup failed
+    alert(error.message);
+  });
+});
+
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+// Assuming you have a Google Login button with the ID "google-login"
+// Assuming you have a Google Login button with the ID "google-login"
+// Assuming you have a Google Login button with the ID "google-login"
+const googleLoginButton = document.getElementById("google-login");
+
+// Attach a click event listener to the Google Login button
+
+// ...
+// Function to fetch tasks from Realtime Database
+function fetchTasks(userUid) {
+  const userTasksRef = ref(database, `googleUsers/${userUid}/tasks`); // Adjust the reference path as needed
+  onValue(userTasksRef, (snapshot) => {
+      const tasksData = snapshot.val() || {};
+      const tasks = Object.keys(tasksData).map((taskId) => ({
+          id: taskId,
+          ...tasksData[taskId],
+      }));
+      renderTasks(tasks);
+  });
+}
+
+
+  googleLoginButton.addEventListener("click", () => {
+    // Authenticate with Firebase using the Google provider object
+    firebase.auth().signInWithPopup(provider)
+        .then((result) => {
+            // The user has successfully signed in with Google
+            // You can access user data from the "result" object
+            const user = result.user;
+            console.log("Google Sign-In successful:", user);
+
+            // Determine the reference based on authentication method
+            const userUid = user.uid;
+            const userTasksRef = ref(database, `googleUsers/${userUid}/tasks`);
+
+            // Fetch the existing tasks from the database
+            fetchTasks(userUid);
+
+            // Store the user's UID in session storage
+            sessionStorage.setItem("user", userUid);
+
+            // Now you can customize the behavior after signing in with Google
+            // For example, you can check if the user's email is verified:
+            if (user.emailVerified) {
+                // User's email is verified, redirect to the desired page
+                window.location.href = "to-do.html";
+            } else {
+                // User's email is not verified, display a message or take appropriate action
+                alert("Please verify your email before continuing.");
+            }
+        })
+        .catch((error) => {
+            // Handle errors if the Google Sign-In fails
+            console.error("Google Sign-In error:", error);
+        });
+  });
+
+  // ...
+
+
+
+
+const forms = document.querySelector(".forms"),
+      pwShowHide = document.querySelectorAll(".eye-icon"),
+      links = document.querySelectorAll(".form-link a");
+
+pwShowHide.forEach(eyeIcon => {
+  eyeIcon.addEventListener("click",() => {
+    let pwfields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+
+    pwfields.forEach(password =>{
+      if (password.type === "password"){
+        password.type = "text";
+        eyeIcon.classList.replace("bx-hide", "bx-show");
+        return;
+      }
+      password.type = "password";
+      eyeIcon.classList.replace("bx-show", "bx-hide");
+    });
+  });  
+});
+
+links.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    forms.classList.toggle("show-signup");
+  });
+});
---
+++ script2.js
@@ -0,0 +1,124 @@
+import { app } from './Firebase/firebase.js';
+import { getDatabase, ref, push, onValue, set } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+
+const database = getDatabase(app);
+
+document.addEventListener('DOMContentLoaded', function () {
+    const taskInput = document.getElementById('task');
+    const addTaskButton = document.getElementById('addTask');
+    const taskList = document.getElementById('taskList');
+
+    // Reference to the logout button
+    const logoutButton = document.getElementById('logout-button');
+
+    // Event listener for the logout button
+    logoutButton.addEventListener('click', () => {
+        // Remove the user from the session storage.
+        sessionStorage.removeItem("user");
+    
+        // Redirect the user to the login page.
+        window.location.href = "index.html";
+    });
+
+    // Function to fetch tasks from Realtime Database
+    function fetchTasks(userUid) {
+        const userTasksRef = ref(database, `${userUid}/tasks`);
+        onValue(userTasksRef, (snapshot) => {
+            const tasksData = snapshot.val() || {};
+            const tasks = Object.keys(tasksData).map((taskId) => ({
+                id: taskId,
+                ...tasksData[taskId],
+            }));
+            renderTasks(tasks);
+        });
+    }
+
+    // Function to save tasks to Realtime Database
+    function saveTasksToDatabase(userUid, tasks) {
+        const userTasksRef = ref(database, `${userUid}/tasks`);
+        const tasksData = tasks.reduce((data, task) => {
+            data[task.id] = { text: task.text, completed: task.completed };
+            return data;
+        }, {});
+        set(userTasksRef, tasksData);
+    }
+
+    let tasks = [];
+
+    // Get the user's UID from session storage
+    const userUid = sessionStorage.getItem("user");
+
+    if (userUid) {
+        // If the user is authenticated, initialize their tasks
+        fetchTasks(userUid);
+    } else {
+        // If the user is not authenticated, redirect them to the login page
+        window.location.href = "index.html";
+    }
+
+    function renderTasks(tasks) {
+        taskList.innerHTML = '';
+        tasks.forEach((task) => {
+            const li = document.createElement('li');
+            li.innerHTML = `
+                <input type="checkbox" class="task-checkbox" data-id="${task.id}" ${task.completed ? 'checked' : ''}>
+                <span class="${task.completed ? 'completed' : ''}">${task.text}</span>
+                <button class="delete" data-id="${task.id}">Delete</button>
+            `;
+            taskList.appendChild(li);
+        });
+    }
+
+    addTaskButton.addEventListener('click', function () {
+        const taskText = taskInput.value.trim();
+    
+        if (taskText !== '') {
+            const newTaskRef = push(ref(database, `${userUid}/tasks`)); // Create a new child node
+            const newTaskId = newTaskRef.key;
+            const newTask = { id: newTaskId, text: taskText, completed: false };
+            tasks.push(newTask);
+            renderTasks(tasks);
+            saveTaskToDatabase(userUid, newTask); // Save the new task
+            taskInput.value = '';
+        }
+    });
+
+    // Function to save a task to Realtime Database
+    function saveTaskToDatabase(userUid, task) {
+        const userTaskRef = ref(database, `${userUid}/tasks/${task.id}`);
+        set(userTaskRef, { text: task.text, completed: task.completed });
+    }
+
+    taskList.addEventListener('click', function (event) {
+        if (event.target.classList.contains('delete')) {
+            const taskId = event.target.getAttribute('data-id');
+            if (taskId) {
+                // Filter out the deleted task
+                tasks = tasks.filter((task) => task.id !== taskId);
+                renderTasks(tasks);
+
+                // Delete the task from the database
+                deleteTaskFromDatabase(userUid, taskId);
+            }
+        } else if (event.target.classList.contains('task-checkbox')) {
+            const taskId = event.target.getAttribute('data-id');
+            if (taskId) {
+                const task = tasks.find((t) => t.id === taskId);
+                if (task) {
+                    task.completed = event.target.checked;
+                    renderTasks(tasks);
+
+                    // Update the task in the database
+                    saveTaskToDatabase(userUid, task);
+                }
+            }
+        }
+    });
+
+    // Function to delete a task from Realtime Database
+    function deleteTaskFromDatabase(userUid, taskId) {
+        const userTaskRef = ref(database, `${userUid}/tasks/${taskId}`);
+        set(userTaskRef, null); // Setting the task to null deletes it from the database
+    }
+
+});
